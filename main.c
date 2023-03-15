0886bc48d7550305d484c72604bdae21423e4c5e
JoaoRava <117782195+JoaoRava@users.noreply.github.com>
2023-03-15 20:28:54 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,27 @@
+/******************************************************************************
+MANIPULANDO VETOR
+LEITURA DOS DADOS DE UM VETOR
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    #define TAM_MAX 10
+    float vet1[TAM_MAX];
+    float maior;
+    int i;
+    for(i=0; i<TAM_MAX; i++)
+        scanf("%f", &vet1[i]);// le os dados
+    
+    maior = vet1[0]; // assumo que o primeiro é o maior
+    for(i=0; i<TAM_MAX; i++)
+        if (vet1[i] > maior)
+            maior = vet1[i];
+            
+    printf("O maior elemento é %f\n", maior);
+    
+    
+
+    return 0;
+}
